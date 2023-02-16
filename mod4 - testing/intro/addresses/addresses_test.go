16a480dfb9dcc40f9b0e74ef706f687cf66b229a
Tyrone Damasceno <tyronedamasceno@gmail.com>
2023-02-16 17:18:36 -0300

    basic testing

--- mod4 - testing/intro/addresses/addresses_test.go
+++ mod4 - testing/intro/addresses/addresses_test.go
@@ -1,12 +1,34 @@
-package addresses
+package addresses_test
 
-import "testing"
+import (
+	"intro/addresses"
+	"testing"
+)
+
+type testScenario struct {
+	inputAddress   string
+	expectedOutput string
+}
 
 func Test_AddressType(t *testing.T) {
-	expected := "Street"
-	result := AddressType("Street Banana")
+	t.Parallel()
 
-	if expected != result {
-		t.Error("Result different from expected")
+	scenarios := []testScenario{
+		{"Street 123", "Street"},
+		{"STREET 567", "Street"},
+		{"Avenue Jackson", "Avenue"},
+		{"Road Black", "Road"},
+		{"My House", "Invalid"},
 	}
+
+	for _, scenario := range scenarios {
+		result := addresses.AddressType(scenario.inputAddress)
+		if result != scenario.expectedOutput {
+			t.Errorf("Result %s is different from expected %s", result, scenario.expectedOutput)
+		}
+	}
+}
+
+func Test_2(t *testing.T) {
+	t.Parallel()
 }
