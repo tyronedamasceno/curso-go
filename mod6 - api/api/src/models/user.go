1a78d30bc1fc9c95ef316223d1200b0f6b022672
Tyrone Damasceno <tyronedamasceno@gmail.com>
2023-04-21 17:45:56 -0300

    Validations

--- mod6 - api/api/src/models/user.go
+++ mod6 - api/api/src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,35 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("name can't be blank")
+	}
+	if user.Nick == "" {
+		return errors.New("nick can't be blank")
+	}
+	if user.Email == "" {
+		return errors.New("email can't be blank")
+	}
+	if user.Password == "" {
+		return errors.New("password can't be blank")
+	}
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
