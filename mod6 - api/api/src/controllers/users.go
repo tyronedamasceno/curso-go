507532561ccf3943d2a36cc3ed7da9b9f291e8a9
Tyrone Damasceno <tyronedamasceno@gmail.com>
2023-04-21 17:12:00 -0300

    Repository

--- mod6 - api/api/src/controllers/users.go
+++ mod6 - api/api/src/controllers/users.go
@@ -1,9 +1,39 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating User"))
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(reqBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUsersRepository(db)
+	userId, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Inserted ID: %d", userId)))
 }
 
 func RetrieveUsers(w http.ResponseWriter, r *http.Request) {
